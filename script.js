c7b4bb8ffc27f273cd06ee5ff0562ca5f0463944
Adunola Johnson <64406918+adun-dotcom@users.noreply.github.com>
2020-10-19 11:09:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var extraNote = document.getElementById("main")
+var createNote = document.getElementById("main")
 var countNote = document.getElementById("count")
 var article = document.getElementsByTagName("article")
 
 
 
-function extraNoteFunction() {
+function createNoteFunction() {
     var addTitle = prompt("Add title here");
     var addNote = prompt("Add note here");
 
     if(addTitle === null || addTitle === ""){
-        extraNote.innerHTML;
+        createNote.innerHTML;
     }
 
     else {
-        extraNote.innerHTML = extraNote.innerHTML + "<article>" + "<h2>" + addTitle + "</h2>" + "<p>" + addNote + "</p>" + "</article>"
+        createNote.innerHTML = createNote.innerHTML + "<article>" + "<h2>" + addTitle + "</h2>" + "<p>" + addNote + "</p>" + "</article>"
     }
 
     countNoteFunction();
