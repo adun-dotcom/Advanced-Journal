dcf670fa58cfa6ff2b1b27df4dca2a9149860f35
Adunola Johnson Odetola <aduntobi7@gmail.com>
2020-10-19 10:56:57 +0100

    changed js.file

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-var main = document.getElementById("main")
+var extraNote = document.getElementById("main")
 var countNote = document.getElementById("count")
 
+var article = document.getElementsByTagName("article")
 
 
 
-function addNoteFunction() {
-    var addNote = prompt("Add note here");
-    
-    main.innerHTML = "<article>" + addNote + "</article>" + main.innerHTML;
-       
-    counterFunction()
+function createNoteFunction() {
+  var dt = new Date();
+
+let year  = dt.getFullYear().toString().substr(-2);
+let month = (dt.getMonth() + 1).toString().padStart(2, "0");
+let day   = dt.getDate().toString().padStart(2, "0");
+
+  
+  var addTitle = prompt("Write title here");
+  var addNote = prompt("Write note here");
+  
+  if (addTitle === null || addTitle === "") {
+    extraNote.innerHTML;
+  }
+  else {
+    extraNote.innerHTML =  "<article>" +"<h2>" + day + "/" + month + "/" + year + ":" + " " +  addTitle + "</h2>" +  "<p>" + addNote + "</p>" + "</article>"  + extraNote.innerHTML ;
+  }
+  
+  counterFunction()
 
     }
     
-    function counterFunction() {
-        var article = document.getElementsByTagName("article").length;
-        countNote.innerHTML = "You have" + " " + article + " " + "notes";
-    
-        
-    } 
-    counterFunction()
-    
+function counterFunction() {
+
+  var counter  =  article.length;
+  countNote.innerHTML = "You have" + " " + counter + " " + "notes";
+  
+}
+counterFunction()    
+
+
+
+
